f21019e1043fac5d08a4799fd63d47f635ff12e7
balalaiQA <balalaiqa88@gmail.com>
2026-09-09 13:29:29 +0300

    build: update Homebrew formula to published Runner 0.1.2

--- Formula/xceasyctl.rb
+++ Formula/xceasyctl.rb
@@ -5,8 +5,8 @@
 class Xceasyctl < Formula
   desc "Runner for XCEasy-based UI tests on Apple devices"
   homepage "https://github.com/qa-point/xceasy-runner"
-  url "https://github.com/qa-point/xceasy-runner/releases/download/v0.1.1/xceasy-runner-0.1.1-macos-universal.tar.gz"
-  sha256 "f7538d878d4ff5b2ab4cd6c686737da204a995f4805090593420cb7f3a8d6af7"
+  url "https://github.com/qa-point/xceasy-runner/releases/download/v0.1.2/xceasy-runner-0.1.2-macos-universal.tar.gz"
+  sha256 "cb232979942e35f7e7c6318cc2a5763e051b7a537d451b5a3b9d27a5dbd9da09"
   license "Apache-2.0"
 
   depends_on "jq"
